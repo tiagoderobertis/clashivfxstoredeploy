fee53e61e74c47a4452ddf5022678f751d2b09f8
Tiago De Robertis <tiaguitodev@gmail.com>
2025-05-06 23:16:20 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-// Configuración optimizada para GitHub Pages
-output: 'export',
-// Desactivar la optimización de imágenes para exportación estática
-images: {
- unoptimized: true,
-},
-// Añadir trailing slash para mejor compatibilidad con GitHub Pages
-trailingSlash: true,
-// Ignorar errores que puedan interrumpir la compilación
-typescript: {
- ignoreBuildErrors: true,
-},
-eslint: {
- ignoreDuringBuilds: true,
-},
-// Asegurarse de que las rutas se generen correctamente
-basePath: '',
-// Asegúrate de que esta línea esté comentada o eliminada
-// assetPrefix: '',
+  // Configuración optimizada para GitHub Pages
+  output: 'export',
+  // Desactivar la optimización de imágenes para exportación estática
+  images: {
+    unoptimized: true,
+  },
+  // Añadir trailing slash para mejor compatibilidad con GitHub Pages
+  trailingSlash: true,
+  // Ignorar errores que puedan interrumpir la compilación
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  // Asegurarse de que las rutas se generen correctamente
+  basePath: '',
 }
 
 export default nextConfig
-
